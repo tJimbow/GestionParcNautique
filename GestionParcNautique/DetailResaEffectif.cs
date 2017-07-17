e9d76810e39e7f207d54a7370e1417d52790a033
tJimbow <triomphe.jeanmarie@gmail.com>
2017-07-17 16:10:28 +0200

    Add class Detail + Reservation

---
+++ GestionParcNautique/DetailResaEffectif.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionParcNautique
+{
+    class DetailResaEffectif
+    {
+        protected int _id;
+        protected int _numSecuEffectif;
+        protected int _idReservation;
+
+        public DetailResaEffectif(int Id, int NumSecuEffectif, int IdReservation)
+        {
+            this._id = Id;
+            this._numSecuEffectif = NumSecuEffectif;
+            this._idReservation = IdReservation;
+        }
+
+    }
+}
---
+++ GestionParcNautique/detailResaEquipement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionParcNautique
+{
+    class DetailResaEquipement
+    {
+        protected int _id;
+        protected int _idEquipement;
+        protected int _idReservation;
+        protected int _quantite;
+
+        public DetailResaEquipement(int Id, int IdEquipement, int IdReservation, int Quantite)
+        {
+            this._id = Id;
+            this._idEquipement = IdEquipement;
+            this._idReservation = IdReservation;
+            this._quantite = Quantite;
+        }
+    }
+}
