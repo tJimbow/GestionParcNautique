d295532688f808370114470405527d4e2d509a0e
tJimbow <triomphe.jeanmarie@gmail.com>
2017-07-19 14:40:02 +0200

    Revert "change"
    This reverts commit 0c414cbd6487dd9bfabcadc47c223af725fbafa6.

---
+++ GestionParcNautique/MenuClient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GestionParcNautique
+{
+    public partial class MenuClient : Form
+    {
+        public MenuClient()
+        {
+            InitializeComponent();
+            MenuAccueil menuAccueil = new MenuAccueil();
+            //this.OnFormClosing(menuAccueil.Show());
+        }
+
+        private void MenuClient_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
