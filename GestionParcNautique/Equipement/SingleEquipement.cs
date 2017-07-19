0c414cbd6487dd9bfabcadc47c223af725fbafa6
tJimbow <triomphe.jeanmarie@gmail.com>
2017-07-19 14:11:53 +0200

    change
    last change

---
+++ GestionParcNautique/Equipement/SingleEquipement.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GestionParcNautique
+{
+    public partial class SingleEquipement : Form
+    {
+        public SingleEquipement()
+        {
+            InitializeComponent();
+        }
+        
+        public void loadData(String Etat, int Puissance, float PrixHT, String Description, String nom)
+        {
+            singleNom.Text = nom;
+            singleEtat.Text = Etat;
+            singlePuissance.Text = Puissance.ToString();
+            singleDescriptif.Text = Description;
+            singlePrixHT.Text = PrixHT.ToString();
+        }
+
+        private void btnLeaveEquipement_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
