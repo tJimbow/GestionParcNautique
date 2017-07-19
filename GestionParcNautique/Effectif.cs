485e89d24c104d224557e965a3cd88dbf7cbecf3
tJimbow <triomphe.jeanmarie@gmail.com>
2017-07-19 15:18:24 +0200

    ajout class effectif

--- GestionParcNautique/Effectif.cs
+++ GestionParcNautique/Effectif.cs
@@ -8,5 +8,21 @@ namespace GestionParcNautique
 {
     class Effectif
     {
+        protected int _numSecuriteSocial;
+        protected DateTime _dateEmbauche;
+        protected DateTime _dateVisiteMedical;
+        protected String _statutContrat;
+        protected int _numPermisCotier;
+        protected String _statutActivite;
+
+        public Effectif(int NumSecuriteSocial, DateTime DateEmbauche, DateTime DateVisiteMedical, string StatutContrat, int NumPermisCotier, string StatutActivite)
+        {
+            this._numSecuriteSocial = NumSecuriteSocial;
+            this._dateEmbauche = DateEmbauche;
+            this._dateVisiteMedical = DateVisiteMedical;
+            this._statutContrat = StatutContrat;
+            this._numPermisCotier = NumPermisCotier;
+            this._statutActivite = StatutActivite;
+        }
     }
 }
